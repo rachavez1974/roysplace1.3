e6aa4f5cfdc363feaeefdf67ac7ffdb278ed5b3f
Ramon Chavez <rachavez1974@gmail.com>
2018-11-07 13:06:00 -0500

    Add static pages and helper for tittle

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
+  get 'static_pages/menus'
+
+  get 'static_pages/offers'
+
+  get 'static_pages/bagged'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#kuca'
+  root 'static_pages#home'
 end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,49 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Roy's Place, a Sandwich Heaven!!"
+  end
+
+  test "should get root" do
+    get root_path
+    assert_response :success
+  end
+
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
+
+  test "should get menus" do
+    get static_pages_menus_url
+    assert_response :success
+    assert_select "title", "Menus | #{@base_title}"
+  end
+
+  test "should get offers" do
+    get static_pages_offers_url
+    assert_response :success
+    assert_select "title", "Offers | #{@base_title}"
+  end
+
+  test "should get bagged" do
+    get static_pages_bagged_url
+    assert_response :success 
+    assert_select "title", "Bagged | #{@base_title}"
+  end
+
+end
