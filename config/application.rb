c2c8dd5fde42afcffacde7fe55db93fe25903d5f
Ramon Chavez <rachavez1974@gmail.com>
2018-11-12 17:11:57 -0500

    try again to display home slider in production

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Roysplace
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.serve_static_assets = true
+    config.assets.compile = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
