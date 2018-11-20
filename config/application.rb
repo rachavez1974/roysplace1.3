d0408bb1a15b9978a8e53741a201a873e997c7e6
Ramon Chavez <rachavez1974@gmail.com>
2018-11-19 22:47:17 -0500

    precompile assets

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Roysplace
     config.load_defaults 5.1
     config.serve_static_assets = true
     config.assets.compile = true
+    config.serve_static_files = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
