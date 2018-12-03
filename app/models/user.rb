6c2505e8ef6058bc9c68a0d7ff0f708e35a7766d
Ramon Chavez <rachavez1974@gmail.com>
2018-12-03 15:29:28 -0500

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   
   #skip downcase_email call back because not needed for in store orders
   #Customer may choose to update their email later and can be done via website portal
   before_save :downcase_email
   before_create :create_activation_digest
+
 
   VAILID_PHONENUMBER_REGEX = /\A\d{10}\z/
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -61,8 +62,25 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
 
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token  = User.new_token
+    update_columns(reset_digest: User.digest(reset_token),
+                   reset_sent_at: Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
   private
-  #makes all emails for User lowercase 
+  # makes all emails for User lowercase 
   def downcase_email
     self.email = email.downcase unless self.email.nil? 
   end
@@ -72,6 +90,8 @@ class User < ApplicationRecord
     self.activation_token  = User.new_token
     self.activation_digest = User.digest(activation_token)
   end
+
+
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   #log in routes for customers and admins
 
   
@@ -30,5 +34,6 @@ Rails.application.routes.draw do
   post 'customer/signup', to: 'users#create'
 
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
 end
